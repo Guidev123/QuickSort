345e62e27cf627c0864edc5f4d427e11fb9119e1
guira <guira@GUILHERME>
2024-04-23 21:30:54 -0300

    Adicionar arquivos de projeto.

---
+++ QuickSort/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace QuickSort
+{
+	public abstract class Program : Sort
+	{
+		static void Main(string[] args)
+		{
+			//Mostra array original
+			int[] nums = { 8, 875, 7, 9, 764, 55 };
+			Console.WriteLine("Array original:");
+			foreach (int i in nums)
+			{
+				Console.WriteLine(i);
+			}
+
+			//Passando array não ordenado, ponteiro esquerdo e ponteiro direito
+			Ordena(nums, 0, nums.Length - 1);
+
+			Console.WriteLine("Array ordenado:");
+
+			foreach (int i in nums)
+			{
+				Console.WriteLine(i);
+			}
+		}
+	}
+}
---
+++ QuickSort/Sort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+	public abstract class Sort
+	{
+		public static void Ordena(int[] nums, int esquerda, int direita)
+		{
+			if (esquerda < direita)
+			{
+				int pivot = SepararArray(nums, esquerda, direita);
+
+				Ordena(nums, esquerda, pivot - 1);
+				Ordena(nums, esquerda + 1, direita);
+			}
+		}
+
+		public static int SepararArray(int[] nums, int esquerda, int direita)
+		{
+			int pivot = nums[direita];
+			int i = esquerda - 1;
+
+			for (int j = esquerda; j < direita; j++)
+			{
+				if (nums[j] <= pivot)
+				{
+					i++;
+					int tempVariable = nums[i];
+					nums[i] = nums[j];
+					nums[j] = tempVariable;
+				}
+			}
+
+			int tempVariable1 = nums[i + 1];
+			nums[i + 1] = nums[direita];
+			nums[direita] = tempVariable1;
+
+			return i + 1;
+		}
+	}
+}
